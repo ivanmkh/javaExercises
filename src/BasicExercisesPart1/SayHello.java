e4b641ce399328c8d86d810f29120efc28abce26
Ivan Mkhitaryan <Ivan.Mkhitaryan@adacta-fintech.com>
2021-07-22 17:19:34 +0300

    init&add first task

---
+++ src/BasicExercisesPart1/SayHello.java
@@ -0,0 +1,9 @@
+package BasicExercisesPart1;
+
+public class SayHello {
+	public static void Run() {
+		var name = "Ivan";
+		System.out.println("Hello!");
+		System.out.println(name);
+	}
+}
---
+++ src/Program.java
@@ -0,0 +1,7 @@
+import BasicExercisesPart1.SayHello;
+
+public class Program {
+	public static void main(String[] args) {
+		SayHello.Run();
+	}
+}
